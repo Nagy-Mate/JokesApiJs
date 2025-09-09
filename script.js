a57e000543a14c8ee665c985ddd1f5f9e7b93386
Nagy Máté <morsakk.mate@gmail.com>
2025-09-09 12:24:52 +0200

    valami

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-function selectType() {
+async function selectType() {
   const type = document.getElementById("type").value;
+  const jokeSetup = document.getElementById("jokeSetup");
+  const jokePunchline = document.getElementById("jokePunchline");
+  const punchButton = document.getElementById("punch");
+
+  punchButton.style.visibility = "hidden";
+  jokeSetup.innerHTML = "";
+  jokePunchline.innerHTML = "";
+
   if(type == "random"){
-    fetch('https://official-joke-api.appspot.com/jokes/random')
-  .then(response => response.json())
-  .then(data => console.log(data));
+    const result = await getData('random');
+  jokeSetup.innerHTML = result.setup;
+  jokePunchline.innerHTML = result.punchline;
+  punchButton.style.visibility = "visible";
+  }else if(type == "id"){
+    const idInput = document.getElementById("jokeIdInput");
+    idInput.style.visibility = "visible";
+    const jokeId = document.getElementById("jokeId").value;
+    
+    async function jokeById(jokeId){
+    const result = await getData(jokeId);
+          jokeSetup.innerHTML = result.setup;
+          jokePunchline.innerHTML = result.punchline;
+          punchButton.style.visibility = "visible";
+    }
+    
   }
 }
+
+function punch(){
+  const jokePunchline = document.getElementById("jokePunchline");
+  jokePunchline.style.visibility = "visible";
+}
+
+async function getData(type) {
+  const url = `https://official-joke-api.appspot.com/jokes/${type}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
